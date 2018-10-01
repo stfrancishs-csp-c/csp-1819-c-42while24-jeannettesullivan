fa7b62290f9adaa7c1b2155e814f8db574b2f894
Jeannette Sullivan <20sullivan@stfrancishs.org>
2018-10-01 19:31:24 +0000

    checking3

--- 42while24.c
+++ 42while24.c
@@ -2,21 +2,20 @@
 int main(void)
 {
     int sum=0;
-    int k;
+    int k=0;
 
-    for (k=0;k<5;k++)
+   while (k<5)
     {
         sum+=k; //use something different in "while" version
+        k++;
+        printf ("k - %2d sum - %4.1d\n",k,sum);
     }
+    //k=5;
 
-    printf ("%d\n",sum);
-
-    k=5;
-
-    while (k>0) {
+    for (k=10;k>0;k--) {
         k=k-1; //use something different in "for" version
-        printf ("%d\n", k);
+        printf ("%6.1d\n", k);
     }
-
+        printf ("Blastoff!");
 }
 
